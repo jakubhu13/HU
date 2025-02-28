dce115ee012f55c83556da6f3e0972f0ad88487e
root <root@kali>
2025-02-28 11:20:20 -0500

    new3

---
+++ main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "WebServer"
+  }
+
+  # Niebezpieczna konfiguracja - otwarte porty dla wszystkich
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Niebezpieczne przechowywanie danych wrażliwych
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "admin"
+  password             = "password123"  # Niebezpieczne - hasło w plain text
+  parameter_group_name = "default.mysql5.7"
+}
